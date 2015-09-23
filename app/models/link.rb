eca3f6c13bc3f6520266b18e359d254326d5f803
Morgan Miller <m.miller722@gmail.com>
2015-09-23 11:52:47 -0600

    add seed data

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ActiveRecord::Base
   belongs_to :user
+
+  enum status: %w(unread read)
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seed
+  def initialize
+    create_user
+    create_links
+  end
+
+  def create_user
+    User.create(email: "mdub@gmail.com", password: "password")
+  end
+
+  def create_links
+    user = User.find_by(email: "mdub@gmail.com")
+
+    link1 = Link.new(url:"https://gist.github.com/worace/0fa8052bfcf983e8722d")
+    link2 = Link.new(url:"http://www.producthunt.com/tech/ubercommute")
+    link3 =   Link.new(url:"http://venturebeat.com/")
+
+    user.links << link1
+    user.links << link2
+    user.links << link3
+  end
+end
+
+Seed.new
